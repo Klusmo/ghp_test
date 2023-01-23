3423bb65b61d3b033e480b522d92c726edfa304d
Augusto Figueiredo <augusto1256@gmail.com>
2023-01-23 09:52:47 -0300

    test modifications

--- src/App.tsx
+++ src/App.tsx
@@ -18,14 +18,14 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Contando {count} cliques
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click no logo do Vite e do React para aprender mais.
       </p>
     </div>
   )
